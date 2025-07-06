913f610a918452a8d4935c8e9d1090a7e29a32b5
Abhinav D <abhinav7647@gmail.com>
2025-07-06 22:27:14 +0530

    initial commit

---
+++ src/test/java/com/practise/smart/expense/tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.smart.expense.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
